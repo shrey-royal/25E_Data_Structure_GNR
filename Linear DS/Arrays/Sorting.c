2825d48e555bbb08733197b85f5d67bf1b6ca07d
Shrey K <srkkadia@gmail.com>
2025-08-08 19:50:16 +0530

    Quick Sort

--- Linear DS/Arrays/Sorting.c
+++ Linear DS/Arrays/Sorting.c
@@ -49,6 +49,45 @@ void insertionSort(int *arr, int size) {
     }
 }
 
+/*
+Partition method which selects a pivot
+and places each element which is less than pivot value to its left
+and the elements greater than the pivot value to its right
+*/
+int partition(int arr[], int lower, int upper) {
+    int i = (lower - 1);
+
+    int pivot = arr[upper]; // select last element as pivot
+
+    int j;
+    for (j = lower; j < upper; j++) {
+        if (arr[j] <= pivot) {
+            // if curent element is smaller than pivot
+            i++;    // increase the index of smaller elements
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[upper]);   // places the last element i.e. the pivot to its correct position
+
+    return (i+1);
+}
+
+/*
+This is where the sorting of the array takes place
+*/
+void quickSort(int arr[], int lower, int upper) {
+    if (upper > lower) {
+
+        // paritioning index is returned by the parition function
+        // element is at its correct position
+        int partitionIndex = partition(arr, lower, upper);
+
+        // sorting elements before and after the partition index
+        quickSort(arr, lower, partitionIndex-1);
+        quickSort(arr, partitionIndex+1, upper);
+    }
+}
+
 void printArray(int *arr, int size) {
     printf("{ ");
     for (int i = 0; i < size; i++) {
@@ -68,7 +107,7 @@ int main() {
     int *arr = (int *)calloc(size, sizeof(int));
 
     for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 100;  // generating random number
+        arr[i] = (rand() % 100000) - 49999;  // generating random number
     }
 
     // printf("\nBefore Sorting: ");
@@ -79,7 +118,8 @@ int main() {
 
     // bubbleSort(arr, size);
     // selectionSort(arr, size);
-    insertionSort(arr, size);
+    // insertionSort(arr, size);
+    quickSort(arr, 0, size-1);
 
     clock_t end = clock();
     double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
@@ -99,7 +139,7 @@ int main() {
 /*
 1. Bubble Sort - 🍋‍🟩
 2. Selection Sort - 🍊
-3. Insertion Sort -
-4. Quick Sort -
-5. Merge Sort -
+3. Insertion Sort -🥭
+4. Quick Sort - 🍉
+5. Merge Sort - 
 */
