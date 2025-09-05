647a79abfacef5405f5392e550f1f09efe61babf
Shrey K <srkkadia@gmail.com>
2025-09-05 20:12:19 +0530

    insertAtPosition

--- Linear DS/Linked List/Singly.c
+++ Linear DS/Linked List/Singly.c
@@ -39,6 +39,31 @@ void insertAtEnd(node** head, int data) {
     }
 }
 
+void insertAtPosition(node** head, int data, int position) {
+    if (position < 1) {
+        printf("Invalid Position.\n");
+        return;
+    }
+
+    node* newNode = createNode(data);
+    if (position == 1) {
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    node* temp = *head;
+    for (int i = 1; i < position - 1; i++) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Position is out of bounds");
+        return;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
 void printList(node* head) {
     printf("\nList: ==> ");
     while (head != NULL) {
@@ -60,9 +85,13 @@ int main() {
     insertAtBeginning(&head, 50);
     insertAtBeginning(&head, 60);
     printList(head);
-    insertAtEnd(&head, 99);
-    insertAtEnd(&head, 98);
-    insertAtEnd(&head, 97);
+    // insertAtEnd(&head, 99);
+    // insertAtEnd(&head, 98);
+    // insertAtEnd(&head, 97);
+    // printList(head);
+    insertAtPosition(&head, 999, 1);
+    insertAtPosition(&head, 333, 2);
+    insertAtPosition(&head, 666, 3);
     printList(head);
 
     return 0;
